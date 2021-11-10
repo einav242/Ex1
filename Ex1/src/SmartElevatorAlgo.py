e6dbce95dc8c8dcdb03db36792360bec81294f70
e2402 <e24022997b@gmail.com>
2021-11-11 00:15:07 +0200

    add class

---
+++ Ex1/src/SmartElevatorAlgo.py
@@ -0,0 +1,27 @@
+from Ex1.src.Building import Building
+
+from Ex1.src.ListOfCallForElevator import ListOfCallForElevator
+
+
+class SmartElevatorAlgo:
+
+    def __init__(self, building, calls, output):
+        self._building = Building(building)
+        self._list_of_call = ListOfCallForElevator(calls)
+        self._output = output
+
+
+   # def elevator_assignment(self):
+        #for k in self._list_of_call:
+
+
+
+
+
+
+
+
+
+
+
+
