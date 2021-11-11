cb6a3f508ced3ddefee383ea1b595c2c5eb838cb
e2402 <e24022997b@gmail.com>
2021-11-11 15:02:49 +0200

    try to write

--- Ex1/src/CallForElevator.py
+++ Ex1/src/CallForElevator.py
@@ -1,10 +1,12 @@
 class CallForElevator:
     def __init__(self, rows):
-        self._str = rows[0]
-        self._time = rows[1]
-        self._src = rows[2]
-        self._dest = rows[3]
-        self._index = rows[5]
+        self.str = rows[0]
+        self.time = rows[1]
+        self.src = rows[2]
+        self.dest = rows[3]
+        self.index = rows[5]
 
     def __iter__(self):
-        return iter([self._str, self._time, self._src, self._dest, self._index])
+        return iter([self.str, self.time, self.src, self.dest, self.index])
+
+
--- Ex1/src/ListOfCallForElevator.py
+++ Ex1/src/ListOfCallForElevator.py
@@ -8,9 +8,17 @@ class ListOfCallForElevator:
         with open(file_csv) as f:
             reader = csv.reader(f)
             self.Call = []
+            my_list = []
             for rows in reader:
-                self.Call.append(CallForElevator(rows))
+                my_list.append(str(rows[0]))
+                my_list.append(float(rows[1]))
+                my_list.append(int(rows[2]))
+                my_list.append(int(rows[3]))
+                my_list.append(int(rows[4]))
+                my_list.append(int(rows[5]))
+                self.Call.append(my_list)
 
     def __iter__(self):
-        for k in self.Call:
-            return iter([self.Call.pop(k)])
+        for k in range(len(self.Call)):
+            for m in range(0, 6):
+                return iter([self.Call[k][m]])
