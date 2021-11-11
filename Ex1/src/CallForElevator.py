de89febc3f4a280a6430e51ddefd47eca3aa520a
e2402 <e24022997b@gmail.com>
2021-11-11 16:34:15 +0200

    version 3

--- Ex1/src/CallForElevator.py
+++ Ex1/src/CallForElevator.py
@@ -4,7 +4,8 @@ class CallForElevator:
         self.time = float(rows[1])
         self.src = int(rows[2])
         self.dest = int(rows[3])
+        self.status = int(rows[4])
         self.index = int(rows[5])
 
     def __iter__(self):
-        return iter([self.str, self.time, self.src, self.dest, self.index])
+        return iter([self.str, self.time, self.src, self.dest,self.status, self.index])
